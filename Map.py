4f3f17cbe08f8cd58c4e27ac55b629b663ffc72e
xababafr <xavier.quelard@gmail.com>
2016-02-24 17:17:54 +0100

    Debug de Map
    toutes les fonctions sont corrigées

--- Map.py
+++ Map.py
@@ -2,16 +2,7 @@
 # C'est un tableau n x n, et chaque case de ce tableau
 # est une liste [indice_sol,objet_animal]
 # indice_sol = 0,1,2 = sol, eau, herbe (ou potentiellement des objets)
-# (avec le polymorphisme c'est meme surement plus interessant d'avoir des objets)
 # objet_animal = instance de la classe concernee
-
-# L = [0,Animal()]
-# type(L[1]) == Animal renvoi True donc easy de check le type d'un animal
-# del(L[1]) fais que l'on obtient juste une liste de taille 1, donc
-# il faudra après le del de animal (pour la mort) remplacer par 0
-# quand on change un animal de position, je suis pas sur que changer une case en [x,0] et la nouvelle en [x,Animal]
-# va conserver le meme animal, et non pas une "copie" ==> pointeurs?! (apres si c'est pas genant OSEF)
-# creer une classe Rien qui permettrait le polymophisme?
 
 # In [16]: a = Animal('gerard')
 # In [17]: L = [0,a]
@@ -25,26 +16,37 @@
 # In [23]: a
 # Out[23]: <__main__.Animal at 0x1097350b8>
 
-#apparamment pas besoin de pointeurs
-#peut etre plusieurs animaux par case
-
-# ou alors si on se sert des refenrences?? en tout cas, il serait pratique d'avoir un objet case
-
-#Créer une méthode ""suppression""
-#Créer une méthode ""deplacer""
-#Créer une méthode ""voisinage""
-#Créer une méthode distance_max
-#Créer une méthode ""distance""
 
 ## truc utile : 
 # a = np.empty((50,50),dtype=object)
 # a[3,2] = Case()
-# voisinnage : a[2:5,1:4] !!!! pratique
 
-# UN ARRAY NUMPY SERAIT + PRATIQUE
+## array-numpy.tolist()
+# pour la vision aux frontieres, 2 solutoion : 
+# 1) riviere geante entourant la map de la taille de la vision max des animaux
+# 2) en dehors c'est le brouillard de guerre, si voisinnage renvoi [], on diminie les visions de 1
+# jusqu'a obtenir un array contenant de vraies infos (onreduit leur vision aux frontieres)
 
-from tkinter import *
 import numpy as np
+from math import sqrt
+
+mappy = [
+    [[0,None],[0,None],[0,None],[0,None],[0,None],[1,None]],
+    [[1,None],[1,None],[0,None],[0,None],[0,None],[1,None]],
+    [[0,None],[0,None],[0,None],[1,None],[0,None],[0,None]],
+    [[0,None],[1,None],[1,None],[0,None],[0,None],[1,None]],
+    [[0,None],[1,None],[0,None],[0,None],[1,None],[0,None]],
+    [[0,None],[1,None],[0,None],[0,None],[0,None],[0,None]]
+]
+
+mappy2 = [
+    [[0,None],[0,None],[0,None],[0,None],[1,None],[0,None]],
+    [[0,None],[0,None],[0,None],[0,None],[1,None],[0,None]],
+    [[0,None],[0,None],[0,None],[0,None],[1,None],[0,None]],
+    [[0,None],[0,None],[0,None],[0,None],[0,None],[0,None]],
+    [[0,None],[0,None],[0,None],[0,None],[1,None],[0,None]],
+    [[0,None],[0,None],[1,None],[1,None],[1,None],[0,None]]
+]
 
 class Map:
 
@@ -55,54 +57,53 @@ class Map:
     # je suppose pr le moment qu'un n'y a qu'un animal par case
 
         
-    def infos(self,x,y):
-        return self.MAP[x,y] # ou alors pk pas surcharger __getitem__()
+    def infos(self,position):
+        return self.MAP[position[0],position[1]] # ou alors pk pas surcharger __getitem__()
         
     # sol = 0
     # herbe = 1
     # eau = 2
         
-## Méthodes reliées à la Class Animal
-
-def voisinage(self,position,vision):
-    # position est un tuple(x,y)
-    # vision est un int 
-    Px = position[0]
-    Py = position[1]
-    V = np.MAP[Px-vision:Px+vision+1,Py-vision:Py+vision+1]
-    return V
-    
-def deplacer(self,position,position2):
-    # position est un tuple (x,y) 
-    # position2 est un tuple (x2,y2)
-    Px = position[0]
-    Py = position[1]
-    Dx = position2[0]
-    Dy = position2[1]    
-    MAP[Dx,Dy][1] = MAP[Px,Py][1]
-    MAP[Px,Py][1] = Rien()
-    
-def suppression(self,position):
-    # position est un tuple (x,y)
-    Px = position[0]
-    Py = position[1]
-    MAP[Px,Py][1] = Rien()
-    
-def distance_max():
-    xmin = 0
-    ymin = 0
-    xmax = len(MAP)
-    ymax = len(MAP)
-    Distmax = sqrt((xmax-xmin)**2+(ymax-ymin)**2)
-    return (Dmax)
-    
-def distance(self,position,position2):
-    # position est un tuple(x,y)
-    # position2 est un tuple (x2,y2)
-    Px = position[0]
-    Py = position[1]
-    Dx = position2[0]
-    Dy = position2[1]
-    Dist = sqrt((Dx-Px)**2+(Dy-Py)**2)
-    return (Dist)
+    # retourne un array vide si on est au bord et que la vision ne va pas sufissament loin
+    def voisinnage(self,position,vision):
+        # position est un tuple(x,y)
+        # vision est un int 
+        Px = position[0]
+        Py = position[1]
+        V = self.MAP[Px-vision:Px+vision+1,Py-vision:Py+vision+1]
+        return V
+        
+    def deplacer(self,position,position2):
+        # position est un tuple (x,y) 
+        # position2 est un tuple (x2,y2)
+        Px = position[0]
+        Py = position[1]
+        Dx = position2[0]
+        Dy = position2[1]    
+        self.MAP[Dx,Dy][1] = self.MAP[Px,Py][1]
+        self.MAP[Px,Py][1] = Rien()
+        
+    def suppression(self,position):
+        # position est un tuple (x,y)
+        Px = position[0]
+        Py = position[1]
+        self.MAP[Px,Py][1] = Rien()
+        
+    def distance_max(self):
+        xmin = 0
+        ymin = 0
+        xmax = len(self.MAP)
+        ymax = len(self.MAP)
+        Dmax = sqrt((xmax-xmin)**2+(ymax-ymin)**2)
+        return (Dmax)
+        
+    def distance(self,position,position2):
+        # position est un tuple(x,y)
+        # position2 est un tuple (x2,y2)
+        Px = position[0]
+        Py = position[1]
+        Dx = position2[0]
+        Dy = position2[1]
+        Dist = sqrt((Dx-Px)**2+(Dy-Py)**2)
+        return (Dist)
 
